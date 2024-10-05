12de2f1d552c2539276283e3075b72b29a9d520e
Jorge1 <agullojorge@gmail.com>
2024-10-05 20:43:01 +0200

    Fix onCreated

--- composeApp/src/androidMain/kotlin/org/example/rickyandmorty/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/rickyandmorty/MainActivity.kt
@@ -1,14 +1,13 @@
 package org.example.rickyandmorty
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
 class MainActivity : ComponentActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         setContent {
             App()
