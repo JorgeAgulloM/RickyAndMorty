6ba69214d8bd8b453c04a28f1dbb913dfb9f7eed
Jorge1 <agullojorge@gmail.com>
2024-10-05 19:55:24 +0200

    Show data in view from viewModel (& Fix Model)

--- composeApp/src/commonMain/kotlin/org/example/rickyandmorty/ui/home/tabs/characters/CharactersScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/rickyandmorty/ui/home/tabs/characters/CharactersScreen.kt
@@ -1,11 +1,12 @@
 package org.example.rickyandmorty.ui.home.tabs.characters
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
 
@@ -13,6 +14,11 @@ import org.koin.core.annotation.KoinExperimentalAPI
 @Composable
 fun CharactersScreen() {
     val viewModel = koinViewModel<CharactersViewModel>()
+    val state: CharactersState by viewModel.state.collectAsStateWithLifecycle()
 
-    Box(modifier = Modifier.fillMaxSize().background(Color.Red))
+    Column(modifier = Modifier.fillMaxSize()) {
+        state.characterOfTheDay?.let {
+            Text(text = it.image)
+        }
+    }
 }
