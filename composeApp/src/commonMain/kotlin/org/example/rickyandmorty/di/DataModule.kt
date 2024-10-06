d96b23670860debe14770a1dd9cc82dd05145f3a
Jorge1 <agullojorge@gmail.com>
2024-10-06 12:29:21 +0200

    Fixed parameter

--- composeApp/src/commonMain/kotlin/org/example/rickyandmorty/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/org/example/rickyandmorty/di/DataModule.kt
@@ -31,6 +31,6 @@ val dataModule = module {
         }
     }
     factoryOf(::ApiService)
-    factory<Repository> { RepositoryImpl(get()) }
+    factory<Repository> { RepositoryImpl(get(), get()) }
     factoryOf(::CharactersPagingSource)
 }
