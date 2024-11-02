545df2552cf8b7e5393be34d1ee1c579cd3f374c
Jorge1 <agullojorge@gmail.com>
2024-11-02 21:21:00 +0100

    CharactersScreen design

--- composeApp/src/commonMain/kotlin/org/example/rickyandmorty/ui/home/tabs/characters/CharactersScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/rickyandmorty/ui/home/tabs/characters/CharactersScreen.kt
@@ -3,18 +3,14 @@ package org.example.rickyandmorty.ui.home.tabs.characters
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
@@ -28,16 +24,21 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.paging.LoadState
 import app.cash.paging.compose.LazyPagingItems
 import app.cash.paging.compose.collectAsLazyPagingItems
 import coil3.compose.AsyncImage
 import org.example.rickyandmorty.domain.model.CharacterModel
+import org.example.rickyandmorty.ui.core.DefaultTextColor
+import org.example.rickyandmorty.ui.core.Green
+import org.example.rickyandmorty.ui.core.components.PagingLoadingState
+import org.example.rickyandmorty.ui.core.components.PagingTypeCustom
+import org.example.rickyandmorty.ui.core.components.PagingWrapper
 import org.example.rickyandmorty.ui.core.extensions.vertical
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
@@ -61,59 +62,25 @@ fun CharactersGridList(
     characterOfTheDay: CharacterModel?,
     navigateToDetail: (CharacterModel) -> Unit
 ) {
-    LazyVerticalGrid(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
-        columns = GridCells.Fixed(2),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
 
-        item(span = { GridItemSpan(2) }) {
+    PagingWrapper(
+        pagingTypeCustom = PagingTypeCustom.LazyVerticalGrid(1),
+        pagingItems = characters,
+        initialView = { PagingLoadingState() },
+        itemView = { CharacterItemList(it) { characterModel -> navigateToDetail(characterModel) } },
+        extraItemsView = {
             Column {
-                Text(text = "Characters", color = Color.Black, fontSize = 24.sp)
+                Text(
+                    "Characters",
+                    color = DefaultTextColor,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Spacer(Modifier.height(6.dp))
                 CharacterOfTheDay(characterOfTheDay)
             }
         }
-
-        when {
-            // Loading Api
-            characters.loadState.refresh is LoadState.Loading && characters.itemCount == 0 -> {
-                item(span = { GridItemSpan(2) }) {
-                    Box(modifier = Modifier.fillMaxHeight().height(100.dp), contentAlignment = Alignment.Center) {
-                        CircularProgressIndicator(Modifier.size(64.dp), color = Color.Green)
-                    }
-                }
-            }
-
-            // Empty Api
-            characters.loadState.refresh is LoadState.NotLoading && characters.itemCount == 0 -> {
-                item { Text(text = "No hay personajes :(") }
-            }
-
-            else -> {
-                // Review Items
-                items(characters.itemCount) { pos ->
-                    characters[pos]?.let { characterModel ->
-                        CharacterItemList(characterModel) { character ->
-                            navigateToDetail(character)
-                        }
-                    }
-                }
-
-                //Waiting Data
-                if (characters.loadState.append is LoadState.Loading) {
-                    item(span = { GridItemSpan(2) }) {
-                        Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator(Modifier.size(64.dp), color = Color.Red)
-                        }
-                    }
-                }
-            }
-        }
-    }
+    )
 }
 
 @Composable
@@ -122,7 +89,7 @@ fun CharacterItemList(characterModel: CharacterModel, onItemSelected: (Character
         modifier = Modifier.clip(RoundedCornerShape(24))
             .border(
                 2.dp,
-                Color.Green,
+                Green,
                 shape = RoundedCornerShape(0, 24, 0, 24)
             ).fillMaxWidth()
             .height(150.dp)
@@ -160,7 +127,7 @@ fun CharacterOfTheDay(characterModel: CharacterModel? = null) {
     Card(modifier = Modifier.fillMaxWidth().height(400.dp), shape = RoundedCornerShape(12)) {
         if (characterModel == null) {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                CircularProgressIndicator(color = Color.Green)
+                CircularProgressIndicator(color = Green)
             }
         } else {
             Box(contentAlignment = Alignment.BottomStart) {
